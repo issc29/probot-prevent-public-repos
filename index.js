571a25ba4d9a91a082fe175c5597a619c1827190
Isaac Cohen <issc29@github.com>
2017-11-21 14:04:37 -0500

    add cc list

--- index.js
+++ index.js
@@ -9,30 +9,6 @@ module.exports = (robot) => {
     robot.log('Privatize Repo: ' + privatize_repo);
 
     return PreventPublicRepos.sync(context.github, context.repo(), context.payload);
-    /*
-    if(!payload.repository.private) {
-      robot.log("Repo Public")
-
-      if(privatize_repo) {
-        robot.log("Making Repo Private")
-
-        const repoName = context.repo().repo
-        const owner = payload.sender.login
-        const params = context.repo({title: '[CRITICAL] Public Repos Disabled', body: 'NOTE: Public Repos are disabled for this organization! Repository was automatically converted to a Private Repo.\n\n/cc @' + owner})
-        context.github.issues.create(params);
-
-        robot.log(repoName)
-        const toPrivateParams = context.repo({private: true, name: repoName})
-        context.github.repos.edit(toPrivateParams);
-      }
-      else {
-        const repoName = context.repo().repo
-        const owner = payload.sender.login
-        const params = context.repo({title: '[CRITICAL] Public Repository Created', body: 'Please note that this repository is publicly visible to the internet!\n\n/cc @' + owner})
-        context.github.issues.create(params);
-      }
-    }
-    */
   });
 
   robot.on('repository.publicized', async context => {
