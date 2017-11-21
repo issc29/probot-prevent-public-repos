a8a1763e410903289b56465d4a82bf5d1b52fe0b
Isaac Cohen <issc29@github.com>
2017-11-21 17:30:18 -0500

    refactor and change logging

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const PreventPublicRepos = require('./lib/PreventPublicRepos')
 
 module.exports = (robot) => {
   robot.on('repository.created', async context => {
-    return PreventPublicRepos.sync(context.github, context.repo(), context.payload);
+    return PreventPublicRepos.sync(context.github, context.repo(), context.payload, robot.log);
   });
 
   robot.on('repository.publicized', async context => {
     robot.log("New repo was publicized");
-    return PreventPublicRepos.sync(context.github, context.repo(), context.payload);
+    return PreventPublicRepos.sync(context.github, context.repo(), context.payload, robot.log);
   });
 }
