61aa64f3ae6a2e85699e983e9b7bdf4f59d4dc55
Isaac Cohen <issc29@github.com>
2017-11-21 15:04:57 -0500

    added monitoring of private to public repos

--- index.js
+++ index.js
@@ -2,16 +2,11 @@ const PreventPublicRepos = require('./lib/PreventPublicRepos')
 
 module.exports = (robot) => {
   robot.on('repository.created', async context => {
-    // Code was pushed to the repo, what should we do with it?
-    //robot.log(context);
-
-    //robot.log('Privatize Repo: ' + privatize_repo);
-
     return PreventPublicRepos.sync(context.github, context.repo(), context.payload);
   });
 
   robot.on('repository.publicized', async context => {
-    // Code was pushed to the repo, what should we do with it?
     robot.log("New repo was publicized");
+    return PreventPublicRepos.sync(context.github, context.repo(), context.payload);
   });
 }
