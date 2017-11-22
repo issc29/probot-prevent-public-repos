5e2908a4ed99d37d512c708c006363745495e318
Isaac Cohen <issc29@github.com>
2017-11-22 14:18:06 -0500

    add test cases to index

--- test/index.test.js
+++ test/index.test.js
@@ -10,19 +10,50 @@ describe('plugin', () => {
     robot = createRobot()
     robot.auth = () => Promise.resolve({})
 
-    event = {
-      event: 'repository',
-      payload: JSON.parse(JSON.stringify(require('./events/repo.create.json')))
-    }
     analyze = jest.fn()
 
     plugin(robot, {}, {analyze})
   })
 
   describe('analyzes on created repo', () => {
+    beforeEach(() => {
+      event = {
+        event: 'repository',
+        payload: JSON.parse(JSON.stringify(require('./events/repo.create.json')))
+      }
+    })
+
     it('analyzes repo', async () => {
       await robot.receive(event)
       expect(analyze).toHaveBeenCalled()
+    })
+  })
+
+  describe('analyzes on publicizied repo', () => {
+    beforeEach(() => {
+      event = {
+        event: 'repository',
+        payload: JSON.parse(JSON.stringify(require('./events/repo.publicizied.json')))
+      }
+    })
+
+    it('analyzes repo', async () => {
+      await robot.receive(event)
+      expect(analyze).toHaveBeenCalled()
+    })
+  })
+
+  describe('does not analyze privatized repo', () => {
+    beforeEach(() => {
+      event = {
+        event: 'repository',
+        payload: JSON.parse(JSON.stringify(require('./events/repo.privatized.json')))
+      }
+    })
+
+    it('analyzes repo', async () => {
+      await robot.receive(event)
+      expect(analyze).not.toHaveBeenCalled()
     })
   })
 })
