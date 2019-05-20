1c7f5f569f50e8a66c7612a9b5af8970c5dfefac
Isaac Cohen <issc29@gmail.com>
2019-05-20 14:59:58 -0400

    Fixed Defaults, Added yaml error handling

--- test/PreventPublicRepos.test.js
+++ test/PreventPublicRepos.test.js
@@ -1,4 +1,5 @@
 const PreventPublicRepos = require('../lib/PreventPublicRepos')
+const yaml = require('js-yaml')
 
 describe('PreventPublicRepos', () => {
   let github
@@ -63,101 +64,156 @@ describe('PreventPublicRepos', () => {
     })
 
     it('publicizied and privateToPublic Disabled', () => {
-      const config = configure(payloadPublicized, `
+      const config = configure(payloadPublicized, yaml.safeLoad(`
         enablePrivateToPublic: false
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).not.toHaveBeenCalled()
     })
 
     it('publicizied and privateToPublic Enabled', () => {
-      const config = configure(payloadPublicized, `
+      const config = configure(payloadPublicized, yaml.safeLoad(`
         monitorOnly: true
         enablePrivateToPublic: true
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are visible to all users',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
     })
 
     it('created and privateToPublic Diabled', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorOnly: true
         enablePrivateToPublic: false
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are visible to all users',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
     })
 
     it('created and privateToPublic Enabled', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorOnly: true
         enablePrivateToPublic: true
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are visible to all users',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
     })
 
     it('created and is excluded Repo', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         excludeRepos: ['test-pro1', 'test-pro']
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).not.toHaveBeenCalled()
     })
 
     it('created and is not excluded Repo', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorOnly: true
         excludeRepos: ['test-pro2', test-pro3]
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are visible to all users',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
     })
 
     it('created private', () => {
-      const config = configure(payloadCreatedPrivate, ``)
+      const config = configure(payloadCreatedPrivate, yaml.safeLoad(``))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).not.toHaveBeenCalled()
     })
 
-    it('created public and monitorOnly mode', () => {
-      const config = configure(payloadCreatedPublic, `
-        monitorOnly: true
-        monitorIssueTitle: 'blah '
-      `)
+    it('created public with defaults', () => {
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(``))
       config.update()
       expect(spyExecutePrivatize).not.toHaveBeenCalled()
       expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are visible to all users',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
+    })
+
+    it('created public and monitorOnly mode', () => {
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
+        monitorOnly: true
+        monitorIssueTitle: 'blah '
+      `))
+      config.update()
+      expect(spyExecutePrivatize).not.toHaveBeenCalled()
+      expect(spyMonitorOnly).toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: 'blah ',
+        body: 'Please note that this repository is publicly visible to all users!\n\n/cc @issc29'
+      })
     })
 
     it('created public and monitorOnly mode disabled', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorOnly: false
-      `)
+      `))
       config.update()
       expect(spyExecutePrivatize).toHaveBeenCalled()
       expect(spyMonitorOnly).not.toHaveBeenCalled()
+      expect(github.issues.create).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        title: '[CRITICAL] Public Repositories are Disabled for this Org',
+        body: 'NOTE: Public Repos are disabled for this organization! Repository was automatically converted to a Private Repo. Please contact an admin to override.\n\n/cc @issc29'
+      })
+      expect(github.repos.edit).toHaveBeenCalledWith({
+        owner: 'issc29-GHfB',
+        repo: 'test-pro',
+        private: true,
+        name: 'test-pro'
+      })
     })
   })
 
   describe('formIssueBody', () => {
     it('formIssueBody with ccList', () => {
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         ccList: "@security"
-      `)
+      `))
       var issueBody = config.formIssueBody('test123', '@security')
       expect(issueBody).toEqual('test123\n\n/cc @issc29\n/cc @security')
     })
 
     it('formIssueBody with no ccList', () => {
-      const config = configure(payloadCreatedPublic, ``)
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(``))
       var issueBody = config.formIssueBody('test123', '')
       expect(issueBody).toEqual('test123\n\n/cc @issc29')
     })
@@ -165,7 +221,7 @@ describe('PreventPublicRepos', () => {
 
   describe('createIssue', () => {
     it('creatIssue with Title and Body', () => {
-      const config = configure(payloadCreatedPublic, ``)
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(``))
       config.createIssue('TitleTest', 'BodyTest')
       expect(github.issues.create).toHaveBeenCalledWith({
         owner: 'issc29-GHfB',
@@ -178,7 +234,7 @@ describe('PreventPublicRepos', () => {
 
   describe('changeVisibility', () => {
     it('changeVisibility of repo', () => {
-      const config = configure(payloadCreatedPublic, ``)
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(``))
       config.changeVisibility()
       expect(github.repos.edit).toHaveBeenCalledWith({
         owner: 'issc29-GHfB',
@@ -193,10 +249,10 @@ describe('PreventPublicRepos', () => {
     it('executeMonitorOnly', () => {
       var spyFormIssueBody = jest.spyOn(PreventPublicRepos.prototype, 'formIssueBody')
       var spyCreateIssue = jest.spyOn(PreventPublicRepos.prototype, 'createIssue')
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorIssueBody: "MonitorIssueBodyText"
         ccList: "@security"
-        `)
+        `))
       config.executeMonitorOnly({
         monitorIssueTitle: 'MonitorIssueTitleText',
         monitorIssueBody: 'MonitorIssueBodyText',
@@ -212,10 +268,10 @@ describe('PreventPublicRepos', () => {
       var spyFormIssueBody = jest.spyOn(PreventPublicRepos.prototype, 'formIssueBody')
       var spyCreateIssue = jest.spyOn(PreventPublicRepos.prototype, 'createIssue')
       var spyChangeVisibility = jest.spyOn(PreventPublicRepos.prototype, 'changeVisibility')
-      const config = configure(payloadCreatedPublic, `
+      const config = configure(payloadCreatedPublic, yaml.safeLoad(`
         monitorIssueBody: "MonitorIssueBodyText"
         ccList: "@security"
-        `)
+        `))
       config.executePrivatize({
         monitorIssueTitle: 'MonitorIssueTitleText',
         monitorIssueBody: 'MonitorIssueBodyText',
